b1104499ccd9bc5250b8d53eee38c52fbc515285
Vovo4k <93855040+Vovo4k@users.noreply.github.com>
2022-12-04 11:39:09 +0200

    Delete vlaicuOSalpha0.2.bat

--- vlaicuOSalpha0.2.bat
+++
@@ -1,222 +0,0 @@
-CDOE::----
-
-
-
-
-@echo off
-color 1b
-cls
-
-:bootscreen
-cls
-set /p password= parol: 
-if %password% == PASSWORD_GOES_HERE goto desktop
-if not %password% == vlaicu goto bootscreentwo
-
-:desktop
-cls
-color 0a
-cls
-echo Welcome to Vlaicu OS Alpha 0.2
-echo.
-echo Date: %date% Time: %time%
-echo vlaicu os is scholl project 2021 vova Good Work :):
-echo 1)zdelai text.
-echo 2)Documenti...
-echo 3)shitalka...
-echo 4)pixelart vodi
-set /p menuselect=
-if %menuselect% == 1 goto write
-if %menuselect% == 2 goto browse
-if %menuselect% == 3 goto calculator
-if %menuselect% == 4 goto pixelart water
-:write
-cls
-echo dobro pozalavat v textovii dokument sdelai textovii dokemnti sdesi ^_^
-echo cacoe imya dash etomu textu?
-set /p writeone= Name:
-cls
-echo Ok, tvoi fail sahronen. veselis ^_^
-pause
-cls
-set /p textone=
-pause
-echo Ok,tvoi fail sdelan na rabochem stole :-)
-pause
-goto desktop
-
-:calculator
-cls
-set /p equ=
-set /a equ=%equ%
-cls
-echo %equ%
-pause
-goto desktop
-
-:chasi
-MZv 
-       яя    %м    @                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      и+ This program requires Microsoft Windows.
-$Zґ	Н!ёLН!                                                                                                                                                                                                                                                                                                                                                                                                                                                                      NE м  LZe    Ь
-      ' @ P µ Х Ы ю                S PP††  Ф  Ђ     ЏA 0рT     Ђ     Р 0рЂ    Ђ     Ь 0рЂ    Z      а  _       CLOCKDATACLOCKCLOCK  ABOUT CLOCKWNDPROC      KERNELGDIUSERяН?  Н?6    #Microsoft Windows Clock Application   mtswslnkmcjXђEU‹мЋШЃ~uяvё Pљяя  ё л	Ѓ~tф+Аѓн‹е]MК
- XђEU‹мЋШѓм,‹F= uй‚ vйл = tT= tZ= t*йх ‹F
-=йtйк я6  ё ™RPяvя6° я6® љяя  ляv
-яvяvяvи|ѓДяvљяя  +А™йВ яvиSѓДлпяvя6 љ?  и+АPљяя  лЦяvЌFЮPљяя  +АPяvЮяvищѓДяvЌFЮPљяя  л«яv
-яvиЊѓДлќиО иj л•яvЌFФPљяя  яv
-я6Ж љяя  яv
-ЌFФPя6Ж љяя  йhя= tО= tБ= t®=uйя=tЎяvяvяv
-яvяvљяя  ѓн‹е]MК
- U‹мё Pљ”  RPљ›  ЈА ё Pљ®  RPљяя  ЈЖ +АPё Pё PљИ  RPљП  Јў +АPё Pё Pљяя  RPљяя  Јє ‹е]ГU‹мя6А љл  я6Ж љф  я6ў љэ  я6є љяя  ‹е]ГU‹мѓмяvљј  ‰FфPё
- Pљ+  ЈВ яvфё Pљ:  ЈД яvфё PљI  ‰Fюяvфё Pљяя  ‰Fьяvяvфљяя  ‹Fю™RPёd ™RPЎВ ™RPис
-RPиK
-ЈМ ‰О ‹Fь™RPёd ™RPЎД ™RPиР
-RPи*
-ЈИ ‰К иЩюѓ>  teя6Є љяя  ЈР ‰Т ЗFъ  ‹FъСаСаР ‹Т ‰Fц‰Vшя6К я6И я6О я6М Д^ц&‹G™RPиs
-RPиНД^ц&‰GяFъѓ~ъ<|№я6Є љяя  ё¤ Pиh
-‹е]ГU‹мёІ P+АPPяvяvљяя  иuѓ~
-u(яvя6 љn  яvя6 ёxжP+АPPљ‚  З  л-ѓ>  t&яvя6 љяя  яvя6 ёиP+АPPљяя  З   ‹е]ГU‹мѓмЌFъPиЮ	ѓДЎЁ 9FюuЎ¦ 9FьuЎ¤ 9Fъt&яvљяя  ‰Fшё Pяvшяvи ѓДяvяvшљW  ‹е]ГU‹мѓмя6Є љ¬  ЈР ‰Т яvё Pљяя  ѓ~ uqяvёІ Pя6Ж љi  яvифѓДё Pя6ў ё ч.¤ ‹ИЎ¦ ™» чыИQяvиыѓД+АPя6ў я6¦ яvизѓДѓ>  tй ё PёP Pя6є я6Ё й„ѓ~tй„ЌFъPирѓДѓ>  u!ЎЁ 9Fюtё PёP Pя6є я6Ё яvиаѓД
-Ў¦ 9FьuЎ¤ 9Fъuйѓ>  uйЏ ё
- PёP Pя6є я6¦ яvиЄѓД
-ё
- PёA Pя6є ё ч.¤ ‹ИЎ¦ ™» чыИQяvиЂѓД
-ё
- PёP Pя6ў ‹FьЈ¦ PяvиdѓД
-ё
- PёA Pя6ў ‹FъЈ¤ № чй‹ИЎ¦ ™» чыИQяvи6ѓД
-л{+АPя6є я6¦ яvиПѓДё Pя6є ё ч.¤ ‹ИЎ¦ ™» чыИQяvи©ѓД+АPя6ў ‹FьЈ¦ Pяvи’ѓДё Pя6ў ‹FъЈ¤ № чй‹ИЎ¦ ™» чыИQяvиhѓДѓ>  u$ЎЁ 9Fюtё PёP Pя6є ‹FюЈЁ PяvиЋѓД
-я6Є љ  ‹е]ГU‹мѓм№ Ў¶ +І чй™‹Д чщ‰Fъя6К я6И я6О я6М ™RPи
-RPиt	‰Fш= }ЗFш ѓ~ъ}ЗFъ ёІ P‹FшСшчШP‹FъСшчШPљяя  Ў¶ +І СшЈ¬ Ўё +ґ Сшґ Јѕ ЎІ ¬ Јј ЗFц  ‹FцСаСаР ‹Т ‰Fт‰Vфё@™RPЎ¬ ™RPД^т&‹G™RPиЉ	RPидѕ ‰Fмё@™RPЎ¬ ™RPД^т&‹™RPиf	RPиАј ‰Fк‹Fц™№ чщТtѓ~ъ~Sѓ~ш|M‹Fк@‰Fо‹Fм@‰Fрл,‹FкFъ‰Fо‹FмFш‰FрЌFкP‹FъСшчШP‹FшСшчШPљяя  яvЌFкPя6А љ7  яFцѓ~ц<}й0яёІ P‹FшСшP‹FъСшPљz  ‹е]ГU‹мѓмяvя6ј я6ѕ љt	  ёd ™RPЎ¬ ™RP‹F
-™RPиЈRPиэ‰Fъ‹FСаСаР ‹Т ‰Fь‰VюяvяvљP  яvяvљ[  яvё@™RP‹Fъ™RPД^ь&‹™RPиURPиЇј Pё@™RP‹Fъ™RPД^ь&‹G™RPи2RPиЊѕ PљЊ	  ‹е]ГU‹мѓмяvё
- Pљяя  яvяvљ&  ѓ~
- tё лё ‰Fь‹F ™№< чщ‰Vю‹Fьч.¬ ™№d чщ‰Fр‹FюСаСаР ‹Т ‰Fк‰Vмё@™RP‹Fр™RPД^к&‹G™RPи¤RPию‰Fцё@™RP‹Fр™RPД^к&‹™RPи„RPиЮ‰Fфѓ~
- tёA лёP ‰Fь‹FСаСаР ‹Т ‰Fж‰Vиё@™RPД^ж&‹G™RP‹Fьч.¬ ™№d чщ™RPи3RPиЌ‰Fъё@™RPД^ж&‹™RP‹Fьч.¬ ™№d чщ™RPи	RPиc‰Fшяv‹Fфј P‹Fцѕ Pљў	  яv‹Fшј P‹Fъѕ Pљє	  яvЎј +FфPЎѕ +FцPљX
-  яv‹Fшј P‹Fъѕ Pљp
-  ѓ~
- tё лё ‰Fь‹F ™№< чщ‰Vю‹Fьч.¬ ™№d чщ‰Fр‹FюСаСаР ‹Т ‰Fв‰Vдё@™RP‹Fр™RPД^в&‹G™RPиERPиџ‰Fъё@™RP‹Fр™RPД^в&‹™RPи%RPи‰Fшяv‹Fфј P‹Fцѕ Pљ†
-  яv‹Fшј P‹Fъѕ Pљћ
-  яvЎј +FфPЎѕ +FцPљяя  яv‹Fшј P‹Fъѕ Pљяя  ‹е]ГU‹мѓмЎ¶ +І ‰FюЎё +ґ ‰Fья6О я6М я6К я6И ™RPи‹RPие‰Fф‰Vц‹Fю™;Vц|%;Fфv‹Fф‰Fъ‹Fю+FъСшІ ‹FъІ Ј¶ л7я6К я6И я6О я6М ‹Fю™RPи9RPи“‰Fш‹Fь+FшСшґ ‹Fшґ Јё ‹е]ГU‹мѓмяvё Pёђ Pё Pљр  ѓ~
- uяvиMѓДАu+Ай;яv
-ёЄ Pё Pљяя  З   ёђ PP+АєМ RPPPPёd P+АPPяvPPљяя  ‰FъPя6 ёиP+АPPљS  АuAё@ Pё( PљН  ‰Fюяvё P‹FюPё( Pљ>  +АP‹FюPёђ PёPљяя  иНхйhя‹FЈ  ёяяєяяRPяvљяя  Ј® ‰° яvё Pёђ Pё PљK
-  яvъ+АPљяя  ‰FьP+АPPPёзPё 	Pљw  яvь+АPёђ PёйPё Pљяя  яvъяvљяя  лЌFиPљяя  ЌFиPљяя  ЌFиP+АPPPљяя  АuЩяvмиыѓД‹е]В
- U‹мѓм
-ё@ Pё Pљяя  ‰Fь‹Шёђ ‰GЊ_‹^ьЗG  ‹^ьЗ ‹^ь‹F‰G
-‹^ьЗGяяЗGяя+АPё ™RPљяя  ‹^ь‰G‹^ьЗG  ‹FьPљяя  Аu+АлEяvьљяя  яvё PЌFцPё Pљяя  яvёђ PЌFцPљяя  ‰FюАtЖяvPљяя  ЈЄ ё ‹е]ГU‹мё ,Н!‹^Ђэ|Ђн3АЉЕ‰ЉБ‰GЉЖ‰G‹е]Г              [+ДsчШ69
- w69 v6Ј ‹аяг°я»°ю»°эPљяя  љяя  Аt+З.   ‰  WVSRи'3АPљяя  Wљяя  Аtи>эPи ёяLН!ГU‹мёяя]ГU‹мёяя]ГU‹м+АPяvљG  ‹е]ГU‹мё@ P‹FчnPљ~  ‹е]ГU‹мяvяvё@ Pљяя  ‹е]ГU‹мяvљЌ  ‹е]ГU‹м+АPяvљШ  ‹е]ГU‹мяvљ6
-  ‹е]ГU‹мяvљц  ‹е]ГU‹мѓм+АP‹F™RPљяя  ‰FюЊЪ‹Р+А‹е]ГU‹мѓмяvљяя  ‰FюЊЪ‹Р+А‹е]ГU‹мяvљяя  ‹е]ГU‹мяvљяя  Аtё л+А‹е]ГU‹м+АPPљяя  ±и«‹е]Г»~ Ѓы„ sSя[CCлтГ» ё DН!r
-цВЂtЂЏR @KyмГґ0Н!Ј4 ГЋ. &‹6, ё 5Н!‰" Њ$ ё %єР
-Н!6‹t г"6Еv ЊЪ3Ы6яr sйIю6Еz ЊЪ» 6яr Г№ 3Ыц‡R tґ>Н!CвтГU‹миPяvи‹е]ГU‹мWVS3я‹FА}чЧ‹VчШчЪ  ‰F‰V‹F
-А}чЧ‹VчШчЪ  ‰F
-‰VАu‹N‹F3Тчс‹Ш‹Fчс‹Ул<ђ‹Ш‹N‹V‹FСлСЩСкСШЫuфчс‹р3ТRPяv
-яvиЬ ;Vwr;Fvѓо3Т‹ЖяtчЪчШѓЪ [^_]В U‹мWV3Ы‹FА}чУ‹VчШчЪ  ‰F‰V‹F
-А}чУ‹VчШчЪ  ‰F
-‰V‹Nчб‹ш‹F‹рчбъ–чfш‹Ч‹ЖЫtчЪчШѓЪ ^_]В 2нгСкСШвъГU‹миh и<э
-дt
-Ђ~ uЖFюЕ" ё %Н!‹t г» яr ‹FґLН!»„ Ѓы„ sSя[CCлтГU‹мV‹F
-‹Nчб‹Ш‹F‹рчбЪ–чfШ‹У‹Ж^]В »„ Ѓы† sSя[CCлтГ; j k § l Ћ q Ш
-   r 0  ]  l     њ 
- ў  | в  ґ    П  н  
-  з    К     З 
- :  ї   Ґ  ы
-  _ ™ I њ  ! " 3 н - ‰ 6 Щ  ' х  ( І ) ‚ * ]
- < m
- = 
- ­ a ° Ѓ ґ µ = &
- 9 € B в E  B Ъ D * H  P  я   эя  Э
- [ X M ‹ N  Q †  W   X  я   
-я  mtswsl                                                                               ЃЃЃ                                       0HPњmtswslnkmc    @ @                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                    яяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяю?яяяяяяю?яяяяяяяяяяяяяяяяяяяяяЗяягяяяяЗяягяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяясяяяяясяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяязсяяяяязсяяя яяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяясяяяяяясяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяЗяягяяяяЗяягяяяяяяяяяяяяяяяяяяяяяю?яяяяяяю?яяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяяmt  @Ђ  ђ K    %  D  яя P‚Vladimir vlaicu vlaicu os 0.2  	      яя  P‚clock     ђ  яя P‚Clock  & " @  яя P‚Version 0.2   / „ 	 яя P‚Copyright © 2021, Fnf team Corp.  5 ;      PЂOk   About...ClockdataToo many clocks or timers.           mtsws  БаDмаoбЁ	Hвµtгџрд^ёжйЗи9лHўн`рЊKуёXц‘Ѓщјь@  D‘ёЁ	Њµ H^9йй9^HџµЊЁ	ё‘D  ?јьЃщ‘XцёKуЊ`рўнHл9Зийёж^рдџtгµHвЁ	oбмаDБа  мајьoбЃщHвXцtгKурд`рёжўнЗиллЗиўнёжaррдKуtгXцHвЃщoбјьма
-:browse
-cls
-
-:pixelart water
-@echo off
-mode 100
-color 1
-echo  ////////////////////++++++++++++++++/////++++++++++++++++oooooooooooooooooooooooooooooooooooooooooss
-echo  ////////////////////+++++++++++++///////+++++++++++++++oooooooooooooooooooooooooooooooooooooooosssss
-echo  /////////////////////++++++++++++++++++++++++++++++++ooooooooooooooooooooooooooooooooooooossssssssss
-echo  ///////////////////+++++++++++++++++++++++++++++++++oooooooooooooooooooooooooooooooooooossssssssssss
-echo  /////////////////++++++++++++++++++++++++++++++++++++ooooooooooooooooooooooooooooooossssssssssssssss
-echo  ////////////+++++++++++++++++++++++++++++++++++++osso+oooooooooooooooooooooooooossssssssssssssssssss
-echo  ////////++++++++++++++++++++++++++++++++oooooooooy+/:/ooooooooooooooooooooooooosssssssssssssssssssss
-echo  /////++++++++++++++++++++++++++++++++++++++++++++o+//+ooooooooooooooooooooooooooosssssssssssssssssss
-echo  ///++++++++++++++++++++++++++++++++++++++++++++++++++ooooooooooooooooooooooooooooossssssssssssssssss
-echo  +++++++++++++++++++++++++++++++++++++++++++oooooooooooooooooooooooooooooooooooooooosssssssssssssssss
-echo  +++++++++++++++++++++++++++++++++o+++++////////++oyys++++++++ooooooooooooooooooooossssssssssssssssss
-echo  +++++++++++++++++++++//++oooo++//////++ooossyyyyyyoo/yyssssoooo++++++oossssssoosssssssssssssssssssss
-echo  ++++++++++++++++++//+ooo+/////+oossyyyhhhdddddddddddhddddddhhhhyyyyssooo+++ossysssosssssssssssssssss
-echo  +++++++++++++++//+ooo/::/++osssyyyyhhhhhhhddddddhhddhddddddddddddhhhhhyyyssoo++ossyssoosssssssssssss
-echo  +++//++//++///+oo+/::/++osssyyyyyhhhhhhhyyyysssssydysyyyyhhhhhhhhhhhhhyyyyyssssoo+oossssosssssssssss
-echo  ////////+///+oo+:::/++oossssyyyyyyyysssssoooo++++os+++++ooosssssssyyyyyyyyyssssssooooossssosssssssss
-echo  //////+////+++/::/++ooossssssssssooooo++/://://++ss+ooo+////+oooossssssssssssssssssoooosssssosssssss
-echo  /////////+++//://+++oooooooooooooo++//::-:///:/++dmmmhs/+oo+++//++ooossssssssssssssssooooosssoosssss
-echo  /////////+++/////++++oooooooooo++++///:-:::/+osydmmmmmhyssoo++o/+++++ooooooooooooooooooooosossosssss
-echo  ++++++///+++////++++++++++++++++++++/:::/+shddddmmmmmdddhhhyyysoooooosooooooooooooooosssssssssssssss
-echo  ++++++++++oo+++oooooo+oo++++++++++++++oshddmmmmmmmmdmmmmdddddhhyyyssssssoooooooooooossyyyyysssyyysyy
-echo  oooooooooooooossoooooooooooooooooooooshddmmmmmmmmmmmmmmmmmmmmmdddhyyssssssooooooooossyyyyyyssyyyssyy
-echo  oooooooossoooossssooo++++++++ooooooossyhdddmmmmmmmddddmmmmmmdddhhyyssssooo++++++oosssyyyyssssyyssssy
-echo  +o++oo+++ooo+++osssooo+++++//+++++++ooosssyyhhddddddddddddhhhyyyssoooo++++++++++oossyyysooosssssssss
-echo  +++++++++++++++++oosooo+++//////////+++++oooosssyyysysssssssssoo+++++////////++ossyyyso+oosssoosssss
-echo  +++++++++++++++++/++ooooo+++////:::::::::///+++++osoo+++++++////::::::////++ossyyyyso+oosssoosssssss
-echo  +++++++++++++++++++++++oossssooo++////:::::/+oossyyss+/::::::::::///+++oossyyyyysooossyyssssssssssss
-echo  ++++++++++++ooo++oooooooooosyyhhhhyyyssssooooo++oso++++++++++ooosssyyyyhhhyysssossyyyyssyyyysyyyyyyy
-echo  +oooooooooooooooosooosssyssssoossyyyhhhhdddddddhhhhhhhhhhhhhhhhdhdhhhyysssossyyyyyyssyyyyyyyyyyyyyyy
-echo  ooooooooooossssssssssssssssyyyyyyyssooooosssssssyyyyyyhhhyyyyyssssssssssyyyyyysssyyyyyyyyyyyyyyyyyyy
-echo  oooosssssssssssssssssssssssssssssssyyyyyyyyyyyyyyyssssssssssyyyyyyyyyssssssssyyyssssyyyyyyyyyyyyyyyy
-echo  oooooooooooooooooooooosssssoossssssssssooooooosssssssssssssssssssssssssssssssssssssssssyyyyyyyyyyyyy
-echo  soooooooooooooooooooooooooooossoooooooooooosssssssssssssyysssssssssssssssssssssssssysyyyyyyyyyyyyyyy
-echo  oooooooooooooooooooooooooooosssssssosssssssssssssysssyyyyyyyyyyyssssssssssssssyyyyyyyyyyyyyyyyyyyyyy
-
-pause >nul
-
-echo 1)%writeone%
-echo 2)%writetwo%
-echo 3)%writethree%
-echo 4)%writefour%
-echo 5)%writefive%
-set /p browse= 
-if %browse% == 1 goto textone
-if %browse% == 2 goto texttwo
-if %browse% == 3 goto textthree
-
-:textone
-cls
-echo %writeone%
-echo %textone%
-pause
-goto desktop
-
-:texttwo
-cls
-echo %writetwo%
-echo %texttwo%
-pause
-goto desktop
-
-:textthree
-cls
-echo %writethree%
-echo %textthree%
-pause
-goto desktop
-
-
-:bootscreentwo
-cls
-echo !Eto Ne Pravilnii Parol!
-echo 1)poprobovat zanovo
-echo 2)viti iz sistemi
-set /p logintype=
-if %logintype% == 1 goto bootscreen
-if %logintype% == 2 goto ltddesktop
-
-:ltddesktop
-cls
-
-MZv 
-       яя    %м    @                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                      и+ This program requires Microsoft Windows.
-$Zґ	Н!ёLН!                                                                                                                                                                                                                                                                                                                                                                                                                                                                      NE м  LZe    Ь
-      ' @ P µ Х Ы ю                S PP††  Ф  Ђ     ЏA 0рT     Ђ     Р 0рЂ    Ђ     Ь 0рЂ    Z      а  _       CLOCKDATACLOCKCLOCK  ABOUT CLOCKWNDPROC      KERNELGDIUSERяН?  Н?6    #Microsoft Windows Clock Application   mtswslnkmcjXђEU‹мЋШЃ~uяvё Pљяя  ё л	Ѓ~tф+Аѓн‹е]MК
- XђEU‹мЋШѓм,‹F= uй‚ vйл = tT= tZ= t*йх ‹F
-=йtйк я6  ё ™RPяvя6° я6® љяя  ляv
-яvяvяvи|ѓДяvљяя  +А™йВ яvиSѓДлпяvя6 љ?  и+АPљяя  лЦяvЌFЮPљяя  +АPяvЮяvищѓДяvЌFЮPљяя  л«яv
-яvиЊѓДлќиО иj л•яvЌFФPљяя  яv
