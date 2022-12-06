cff1fbcb6ad5af1c82ade85f09825c1f7f731e1a
Vovo4k <93855040+Vovo4k@users.noreply.github.com>
2022-12-06 18:39:15 +0200

    Add files via upload

---
+++ System/CoreServices/Moonlight_app/Contents/MacOS/tipue_set.js
@@ -0,0 +1,65 @@
+// Tipue JS 4.1
+//
+// Tipue JS Copyright (C) 2002-2006 Tri-State Consultants
+// Tipue JS is open source and released under the GNU General Public License
+//
+// Tipue JS Set
+
+
+// General
+
+
+data = 'tipue_data.xml';
+data_source = 2;
+results_location = 'moonlight_results.html';
+
+
+// Results 
+
+
+per_page = 100;
+context = 0;
+context_seed = 10;
+descriptive_length = 100000;
+rank_seed = 3;
+include_num = 0;
+include_url = 0;
+header_links = 0;
+bold_query = 0;
+bold_title = 0;
+bold_footer = 0;
+
+
+// Search
+
+
+common_words = '';
+stemming = 'javascript^js checkboxes^checkbox';
+spell = 'tipu^Tipue endshet^Endsheet';
+
+
+// Text
+
+
+tl_1 = '<table width=100% bgcolor=#f1f1f1 border=0 align=center><tr><td valign=middle align=center><font face=\"Lucida Grande, Tahoma\" size=1>No results found for</td></tr></table>';
+tl_2 = '';
+tl_3 = 'Previous';
+tl_4 = 'Next';
+tl_5 = 'to';
+tl_6 = 'of';
+tl_7 = 'for';
+tl_8 = 'Did you mean:';
+tl_9 = '';
+tl_10 = '';
+tl_11 = '';
+tl_12 = '';
+
+
+// XML
+
+
+xml_pages = 'page';
+xml_title = 'title';
+xml_url = 'url';
+xml_content = 'content';
+
