462a728b760bdbc9d026556c3c6bcaf44e25a680
Vladimir Vlaicu <93855040+Vovo4k@users.noreply.github.com>
2023-01-20 15:07:16 +0200

    Add files via upload

---
+++ release/Commands/CommandManager.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VlaicuOS.Commands
+{
+    public class CommandManager
+    {
+
+        private List<Command> commands;
+
+        public CommandManager() { 
+
+            this.commands=new List<Command>(1);
+            this.commands.Add(new Help("help"));
+
+        }
+
+        public String proccesInput (String input)
+        {
+            String[] split = input.Split(' ');
+
+            String label = split[0];
+
+            List<String> args=new List<string>();
+
+            int ctr = 0;
+            foreach (String s in split) {
+
+
+                if (ctr!=0)
+                    args.Add(s);
+
+                ++ctr;
+
+            }
+
+            foreach (Command cmd in this.commands)
+            {
+
+                if (cmd.name == label)
+                    return cmd.execute(args.ToArray());
+
+            }
+
+            return "Your Command \""+label+"\" does not exist!";
+
+        }
+
+    }
+}
---
+++ release/Commands/Help.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VlaicuOS.Commands
+{
+    public class Help : Command
+    {
+
+        public Help (String name) : base (name) { }
+
+        public override String execute (String[] args)
+        {
+            return "Welcome to VlaicuOS  it is beta only help command OMG";
+        }
+
+    }
+}
