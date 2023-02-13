95711cc6fe2c6246b4f1bad5e98746ab7b224843
Vladimir Vlaicu <93855040+Vovo4k@users.noreply.github.com>
2023-02-13 17:08:25 +0200

    Add files via upload

--- Compile.sh
+++ Compile.sh
@@ -1,1 +1,3 @@
-nasm -fbin -o VlaicuLoader.bin VlaicuLoader.asm
+nasm -fbin -o VlaicuLoader.bin VlaicuLoader.asmm
+dd if=/dev/zero of=VlaicuOS.img bs=1024 count 1440
+dd if=VlaicuLoader.bin of=VlaicuOS.img seek=0 count=1 conv=notrunc
