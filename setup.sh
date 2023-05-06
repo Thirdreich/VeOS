ab2dfa67e672ce2140836c6ab374e7429884a85e
Vladimir Vlaicu <93855040+Vovo4k@users.noreply.github.com>
2023-05-06 09:57:38 +0300

    Delete setup.sh

--- setup.sh
+++
@@ -1,8 +0,0 @@
-echo "Installing tools..."
-haxelib install flixel
-haxelib install lime
-haxelib install openfl
-haxelib run lime setup
-haxelib install flixel-tools
-haxelib run flixel-tools setup
-echo "Done !"
